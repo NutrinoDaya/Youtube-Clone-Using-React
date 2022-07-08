4717541fdb7123a5a4dcdf7030db106e01eb0631
Mohammad Dayarneh <93088580+NutrinoDaya@users.noreply.github.com>
2022-07-08 14:14:32 +0200

    Update Sidebariconfunc.js

--- src/Sidebariconfunc.js
+++ src/Sidebariconfunc.js
@@ -2,47 +2,7 @@ import React from 'react'
 import './Slidebar.css'
 
 //function Sidebariconfunc({selected,Icon,Title})
-export function Sidebariconfunc1({ActiveIcon,InactiveIcon,isActive,Title}) {
-  return (
-    <div className='SidebarRow'>
-      <div className='Sidebar_Icon'>
-        {isActive ? <ActiveIcon/> : <InactiveIcon />}
-      </div>
-      <div className='Slidebar_Title'>{Title}</div>
-    </div>
-  )
-}
-export function Sidebariconfunc2({ActiveIcon,InactiveIcon,isActive,Title}) {
-  return (
-    <div className='SidebarRow'>
-      <div className='Sidebar_Icon'>
-        {isActive ? <ActiveIcon/> : <InactiveIcon />}
-      </div>
-      <div className='Slidebar_Title'>{Title}</div>
-    </div>
-  )
-}
-export function Sidebariconfunc3({ActiveIcon,InactiveIcon,isActive,Title}) {
-  return (
-    <div className='SidebarRow'>
-      <div className='Sidebar_Icon'>
-        {isActive ? <ActiveIcon/> : <InactiveIcon />}
-      </div>
-      <div className='Slidebar_Title'>{Title}</div>
-    </div>
-  )
-}
-export function Sidebariconfunc4({ActiveIcon,InactiveIcon,isActive,Title}) {
-  return (
-    <div className='SidebarRow'>
-      <div className='Sidebar_Icon'>
-        {isActive ? <ActiveIcon/> : <InactiveIcon />}
-      </div>
-      <div className='Slidebar_Title'>{Title}</div>
-    </div>
-  )
-}
-export function Sidebariconfunc5({ActiveIcon,InactiveIcon,isActive,Title}) {
+export function Sidebariconfunc({ActiveIcon,InactiveIcon,isActive,Title}) {
   return (
     <div className='SidebarRow'>
       <div className='Sidebar_Icon'>
